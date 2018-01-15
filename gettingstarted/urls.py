eaa61284f826cae791b422a97a768801316616e8
Alicia T. Singham Goodwin <asinghamgoodwin@gmail.com>
2018-01-15 17:48:37 -0500

    remove references to admin

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import include, url
 from django.urls import path
 
-from django.contrib import admin
-admin.autodiscover()
+# from django.contrib import admin
+# admin.autodiscover()
 
 import train_times.views
 
